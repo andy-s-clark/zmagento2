62bd4a89aa07e10aa00374e94f8959e7b1e104c6
Andy Clark <andy.clark@build.com>
2013-05-16 17:06:31 -0700

    Started a debugging module

---
+++ packages/Buildcom/Debug/controllers/IndexController.php
@@ -0,0 +1,41 @@
+<?php
+class Buildcom_Debug_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        $edition = Mage::getEdition();
+        $version = Mage::getVersion();
+        echo '<h1><em>' . htmlentities($edition) . '</em> Edition, Version <em>' . htmlentities($version) . '</em></h1>' . PHP_EOL;
+    }
+
+    public function basicAction() {
+        $config = Mage::getConfig();
+        echo '<h2>Areas</h2>' . PHP_EOL;
+        $areas = $config->getAreas();
+        foreach( $areas as $key => $value ) {
+            echo '<h3>' . htmlentities($key) . '</h3>' . PHP_EOL;
+            var_dump($value);
+        }
+        unset($areas);
+
+        echo '<h2>Routers</h2>' . PHP_EOL;
+        $routers = $config->getRouters();
+        var_dump($routers);
+
+        $params = $this->getRequest()->getParams();
+        if ( ! empty($params) ) {
+            var_dump($params);
+        }
+    }
+
+    public function configAction() {
+        $config = Mage::getConfig();
+        header('Content-Type: text/xml');
+        echo $config->getNode()->asXML();
+    }
+
+    public function designAction() {
+        $design = Mage::getDesign();
+        echo 'Theme: ' . $design->getDesignTheme()->getFullPath() . '<br />' . PHP_EOL;
+    }
+}
